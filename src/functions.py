c46529753b7787dd3256570c9d4ca8ac96a1db6e
SeWa <se.wa@live.de>
2026-02-14 23:21:42 +0100

    inline markdown

--- src/functions.py
+++ src/functions.py
@@ -1,5 +1,5 @@
 from textnode import TextNode, TextType
-
+import re
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
     for old_node in old_nodes:
@@ -13,3 +13,31 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 new_nodes.append(TextNode(sub_string,old_node.text_type))
 
     return new_nodes
+
+def extract_markdown_images(text):
+    result = []
+    regex_alt = r"!\[.*?\)"
+    regex_ballast = r"[!\[\]\)]"  
+    alt_list = re.findall(regex_alt, text)
+
+    for i in range (0, len(alt_list)):
+        sanitizedstring = re.sub(regex_ballast, '', alt_list[i])
+        splitstring = sanitizedstring.split("(")
+        result.append((splitstring[0], splitstring[1]))
+
+
+    return result
+
+def extract_markdown_links(text):
+    result = []
+    regex_alt = r"[^!]\[.*?\)"
+    regex_ballast = r"[!\[\]\)]"  
+    alt_list = re.findall(regex_alt, text)
+
+    for i in range (0, len(alt_list)):
+        sanitizedstring = re.sub(regex_ballast, '', alt_list[i])
+        splitstring = sanitizedstring.split("(")
+        result.append((splitstring[0], splitstring[1]))
+
+
+    return result
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,5 +1,5 @@
 import unittest
-from functions import split_nodes_delimiter
+from functions import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
 from textnode import TextNode, TextType, text_node_to_html_node
 
 
@@ -96,6 +96,18 @@ class TestTextNode(unittest.TestCase):
         text_type = TextType.BOLD
         new_nodes = split_nodes_delimiter(old_nodes, delimiter, text_type)
         self.assertEqual(new_nodes, old_nodes)
+
+    def test_extract_markdown_link(self):
+        matches = extract_markdown_links(
+            "This is text with an [link](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([(" link", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
 
 if __name__ == "__main__":
     unittest.main()
